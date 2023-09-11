328880c133c547ba8cbf94d2aa7f09101ba8c8df
AlexAlves777 <144742343+AlexAlves777@users.noreply.github.com>
2023-09-11 20:57:05 -0300

    criação da pagina

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const entrada = document.getElementById("entrada");
 const btn = document.getElementById("btn");
+let numSacreto = 89;
 
 btn.addEventListener("click", function() { 
     if(entrada.value =="")return alert ("Digite um numero!")
     console.log( entrada.value)
 
-})
+    if (entrada.value > numSacreto) {
+        alert("O numero é maior!")
+    }
+
+    else if(entrada.value < numSacreto) {
+        alert("O numero é menor!")   
+    }
+    
+    else {
+        alert("Você acertou")
+    }
+        
+    }
+)
